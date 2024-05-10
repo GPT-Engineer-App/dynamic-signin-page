86d3d44cdcdcbbb34d029f2f7d11e4d38e6353fb
xforgexcoderofficial <xforgexxcoder22@gmail.com>
2024-05-10 10:51:50 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ const theme = extendTheme({
   styles: {
     global: (props) => ({
       body: {
-        bg: props.colorMode === "dark" ? "url('https://wallpapers.com/images/hd/rei-ogami-anime-profile-qcj1kvbaici8vgho.jpg')" : "url('https://api.wallpapers.ai/static/downloads/bf459d59d8904f08841e2c1d9a78ceae/upscaled/000000_43196607_kdpmpp2m15_PS7.5_4k_wallpaper_minimalistic_anime_girl_with_silver_hair_and_dark_green_eyes_on_left_side_picture._digital_art_concept_art_[upscaled].jpg')",
+        bg: props.colorMode === "dark" ? "src='/images/darkmode.jpg'" alt="Logo'" : "url('https://api.wallpapers.ai/static/downloads/bf459d59d8904f08841e2c1d9a78ceae/upscaled/000000_43196607_kdpmpp2m15_PS7.5_4k_wallpaper_minimalistic_anime_girl_with_silver_hair_and_dark_green_eyes_on_left_side_picture._digital_art_concept_art_[upscaled].jpg')",
         backgroundSize: "cover",
         transition: "background 0.3s ease-out",
       },
