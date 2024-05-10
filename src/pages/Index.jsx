8445918154885f38393235acaaef2d06aade566c
xforgexcoderofficial <xforgexxcoder22@gmail.com>
2024-05-10 11:00:04 +0100

    Update Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { Container, VStack, Input, Button, FormControl, FormLabel, InputGroup, InputRightElement, IconButton, useColorMode, useColorModeValue, Heading, Text, Link, Switch, FormHelperText, Box } from "@chakra-ui/react";
 import { FaEye, FaEyeSlash, FaUser, FaLock, FaMoon } from "react-icons/fa";
 
