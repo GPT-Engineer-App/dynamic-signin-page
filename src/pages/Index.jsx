3672042f291dc328be019f537a5a2a096a35b03a
xforgexcoderofficial <xforgexxcoder22@gmail.com>
2024-05-10 14:10:05 +0100

    Update Index.jsx

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -72,19 +72,7 @@ const Index = () => {
             {passwordError && <Alert status="error">{passwordError}</Alert>}
           </FormControl>
 
-          {showHomeContainers && (
-            <Flex direction="row" justify="space-around" w="100%">
-              <Box w="30%" bg="red.200" p="4">
-                Container 1
-              </Box>
-              <Box w="30%" bg="green.200" p="4">
-                Container 2
-              </Box>
-              <Box w="30%" bg="blue.200" p="4">
-                Container 3
-              </Box>
-            </Flex>
-          )}
+
           <Button colorScheme="blue" w="80%" bgColor={useColorModeValue("#a5b9b2", "#061c52")} color="white" fontWeight="bold">
             Sign In
           </Button>
